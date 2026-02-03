2536829556eddb45585295aadbcb63facf672bdb
Anthony <thonyrivera9@gmail.com>
2026-02-03 12:18:32 -0500

    Update backend URL to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-dominio.com/api' // cambiar cuando despliegues
+  apiUrl: 'https://gastosback.mercadodigitalhuanuco.com/api'
 };
